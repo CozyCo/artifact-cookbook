c2763a8c7b7cbbbedd6e4caaa10877aa3cc9eabc
Jeremy Kitchen <jeremy@cozy.co>
2019-04-26 13:33:31 -0700

    write out the file
    for some reason the old method was writing a 0 byte file. this seems to
    do the needful

--- libraries/chef_artifact.rb
+++ libraries/chef_artifact.rb
@@ -98,12 +98,8 @@ class Chef
           object = get_s3_object(bucket_name, object_name)
 
           Chef::Log.debug("Downloading #{object_name} from S3 bucket #{bucket_name}")
-          ::File.open(destination_file, 'wb') do |file|
-            object.body.read do |chunk|
-              file.write(chunk)
-            end
-            Chef::Log.debug("File #{destination_file} is #{file.size} bytes on disk")
-          end
+          File.write(destination_file, object.body.read)
+          Chef::Log.debug("File #{destination_file} is #{File.size(destination_file)} bytes on disk")
         rescue URI::InvalidURIError
           Chef::Log.warn("Expected an S3 URL but found #{source_file}")
           raise
